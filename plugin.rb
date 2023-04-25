c7df6b4d21d127406217fadb12fa8117fc9619c3
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2023-04-25 10:26:18 -0400

    test

--- plugin.rb
+++ plugin.rb
@@ -17,10 +17,46 @@ require_relative 'auth/genesys_cloud_authenticator'
 # require_relative 'auth/genesys_cloud_apse2_authenticator'
 # require_relative 'auth/genesys_cloud_sae1_authenticator'
 
-require_relative 'omniauth-genesys-cloud.rb'
+# require_relative 'omniauth-genesys-cloud.rb'
 
 #puts can be found on the docker container in /var/www/discourse/log/unicorn.stdout.log
 puts 'Genesys Cloud - plugin loaded'
+
+class ::OmniAuth::Strategies::Oauth2Basic < ::OmniAuth::Strategies::OAuth2
+  option :name, "genesysCloud"
+
+  uid do
+    if path = SiteSetting.oauth2_callback_user_id_path.split(".")
+      recurse(access_token, [*path]) if path.present?
+    end
+  end
+
+  info do
+    if paths = SiteSetting.oauth2_callback_user_info_paths.split("|")
+      result = Hash.new
+      paths.each do |p|
+        segments = p.split(":")
+        if segments.length == 2
+          key = segments.first
+          path = [*segments.last.split(".")]
+          result[key] = recurse(access_token, path)
+        end
+      end
+      result
+    end
+  end
+
+  def callback_url
+    Discourse.base_url_no_prefix + script_name + callback_path
+  end
+
+  def recurse(obj, keys)
+    return nil if !obj
+    k = keys.shift
+    result = obj.respond_to?(k) ? obj.send(k) : obj[k]
+    keys.empty? ? result : recurse(result, keys)
+  end
+end
 
 FRAME_WIDTH = 600
 FRAME_HEIGHT = 543
