d83c329e65cfc39c2302bb4c3466a00163933242
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2023-04-25 17:08:55 -0400

    test

--- auth/genesys_cloud_authenticator.rb
+++ auth/genesys_cloud_authenticator.rb
@@ -44,10 +44,85 @@ class ::GenesysCloudAuthenticator < Auth::ManagedAuthenticator
                       }
   end
 
+  # def json_walk(result, user_json, prop, custom_path: nil)
+  #   path = custom_path || SiteSetting.public_send("oauth2_json_#{prop}_path")
+  #   if path.present?
+  #     #this.[].that is the same as this.that, allows for both this[0].that and this.[0].that path styles
+  #     path = path.gsub(".[].", ".").gsub(".[", "[")
+  #     segments = parse_segments(path)
+  #     val = walk_path(user_json, segments)
+  #     result[prop] = val if val.present?
+  #   end
+  # end
+
+  # def parse_segments(path)
+  #   segments = [+""]
+  #   quoted = false
+  #   escaped = false
+
+  #   path
+  #     .split("")
+  #     .each do |char|
+  #       next_char_escaped = false
+  #       if !escaped && (char == '"')
+  #         quoted = !quoted
+  #       elsif !escaped && !quoted && (char == ".")
+  #         segments.append +""
+  #       elsif !escaped && (char == '\\')
+  #         next_char_escaped = true
+  #       else
+  #         segments.last << char
+  #       end
+  #       escaped = next_char_escaped
+  #     end
+
+  #   segments
+  # end
+
+  def log(info)
+    Rails.logger.warn("OAuth2 Debugging: #{info}")
+  end
+
+
+
+  # def walk_path(fragment, segments, seg_index = 0)
+  #   first_seg = segments[seg_index]
+  #   return if first_seg.blank? || fragment.blank?
+  #   return nil unless fragment.is_a?(Hash) || fragment.is_a?(Array)
+  #   first_seg = segments[seg_index].scan(/([\d+])/).length > 0 ? first_seg.split("[")[0] : first_seg
+  #   if fragment.is_a?(Hash)
+  #     deref = fragment[first_seg] || fragment[first_seg.to_sym]
+  #   else
+  #     array_index = 0
+  #     if (seg_index > 0)
+  #       last_index = segments[seg_index - 1].scan(/([\d+])/).flatten() || [0]
+  #       array_index = last_index.length > 0 ? last_index[0].to_i : 0
+  #     end
+  #     if fragment.any? && fragment.length >= array_index - 1
+  #       deref = fragment[array_index][first_seg]
+  #     else
+  #       deref = nil
+  #     end
+  #   end
+
+  #   if (deref.blank? || seg_index == segments.size - 1)
+  #     deref
+  #   else
+  #     seg_index += 1
+  #     walk_path(deref, segments, seg_index)
+  #   end
+  # end
+
   def fetch_user_details(token)
     user_json_url = "https://api.#{@region}/api/v2/users/me?expand=organization"
-    user_json = JSON.parse(open(user_json_url, 'Authorization' => "Bearer #{token}" ).read)
-    puts user_json
+    bearer_token = "Bearer #{token}"
+    connection = Faraday.new { |f| f.adapter FinalDestination::FaradayAdapter }
+    headers = { "Authorization" => bearer_token, "Accept" => "application/json" }
+    user_json_response = connection.run_request(user_json_method, user_json_url, nil, headers)
+
+    log("user_json_response: #{user_json_response.inspect}")
+
+    user_json = JSON.parse(user_json_response.body)
 
     result = {
       :name     => user_json['name'],
