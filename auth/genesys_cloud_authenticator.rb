680f273b6609b8ac4590faef8a7b7e034ef2b5cb
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2023-04-27 15:25:19 -0400

    test

--- auth/genesys_cloud_authenticator.rb
+++ auth/genesys_cloud_authenticator.rb
@@ -107,11 +107,13 @@ class GenesysCloudAuthenticator < Auth::ManagedAuthenticator
         puts "Sync happend"
 	    	query = "SELECT user_id FROM email_tokens WHERE email='" + result.email.downcase + "' ORDER BY id DESC LIMIT 1"
 	    	email_user_object = ActiveRecord::Base.connection.exec_query(query)
-        puts email_user_object["user_id"]
-        log(email_user_object)
+        exam = email_user_object.to_ary
+        puts exam[0]
+        puts exam
+      
 	    	if email_user_object != nil
           puts "User Id from database:"
-          puts email_user_object[:user_id]
+          puts email_user_object["user_id"]
 	    		result.user = User.where(id: email_user_object["user_id"]).first
 	    	end
         puts result.user
