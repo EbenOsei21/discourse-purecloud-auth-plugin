6512d3b3326c9edd89445171995d4906e2e009e5
Ebenezer Osei <Ebenezer.Osei@genesys.com>
2023-05-03 15:28:47 -0400

    test

--- auth/genesys_cloud_authenticator.rb
+++ auth/genesys_cloud_authenticator.rb
@@ -61,7 +61,38 @@ class GenesysCloudAuthenticator < Auth::ManagedAuthenticator
     result
   end
 
+  def match_by_email
+    true
+  end
+
+  def find_user_by_email(auth_token)
+    email = auth_token.dig(:info, :email)
+    User.find_by_email(email)
+  end
+
   def after_authenticate(auth)
+
+    association =
+      UserAssociatedAccount.find_or_initialize_by(
+        provider_name: auth[:provider],
+        provider_uid: auth[:uid],
+    )
+
+    if match_by_email && association.user.nil? && (user = find_user_by_email(auth))
+      UserAssociatedAccount.where(user: user, provider_name: auth[:provider]).destroy_all # Destroy existing associations for the new user
+      association.user = user
+    end
+
+    association.info = auth[:info] || {}
+    association.credentials = auth[:credentials] || {}
+    association.extra = auth[:extra] || {}
+
+    association.last_used = Time.zone.now
+
+    # Save to the DB. Do this even if we don't have a user - it might be linked up later in after_create_account
+    association.save!
+
+
 	  result = Auth::Result.new
   	
   	begin
@@ -75,17 +106,19 @@ class GenesysCloudAuthenticator < Auth::ManagedAuthenticator
 	    # Genesys Cloud doesn't have a concept of a validated email
 	    result.email_valid = false
 
-	    current_info = ::PluginStore.get(@provider_name, "#{@provider_name}_user_#{user_details[:user_id]}")
-	    if current_info
-	      result.user = User.where(id: current_info[:user_id]).first
-        puts "user exits"
-        puts result.user
-	    end
+	    # current_info = ::PluginStore.get(@provider_name, "#{@provider_name}_user_#{user_details[:user_id]}")
+	    # if current_info
+	    #   result.user = User.where(id: current_info[:user_id]).first
+      #   puts "user exits"
+      #   puts result.user
+	    # end
 
 	    result.extra_data = {
         purecloud_user_id: user_details[:user_id],
         purecloud_org_id: user_details[:org_id]
 	    }
+
+      result.user = association.user
 
 			####### BEGIN EMPLOYEE SYNC
 	    #Special logic for the prod genesys org
@@ -103,6 +136,15 @@ class GenesysCloudAuthenticator < Auth::ManagedAuthenticator
   end
 
   def after_create_account(user, auth)
-    ::PluginStore.set(@provider_name, "#{@provider_name}_user_#{auth[:extra_data][:purecloud_user_id]}", {user_id: user.id })
+    auth_token = auth_result[:extra_data]
+    association =
+      UserAssociatedAccount.find_or_initialize_by(
+        provider_name: auth_token[:provider],
+        provider_uid: auth_token[:uid],
+      )
+    association.user = user
+    association.save!
+    auth_result.apply_associated_attributes!
+    # ::PluginStore.set(@provider_name, "#{@provider_name}_user_#{auth[:extra_data][:purecloud_user_id]}", {user_id: user.id })
   end
 end
